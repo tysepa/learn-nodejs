d247a2ce49f1039463a8b8e0737b39289cc58222
tysepa <tysepa88@gmail.com>
2022-05-10 21:24:10 +0200

    update on course

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -28,3 +28,40 @@ readFile('./content/first.txt','utf8',(err, result)=>{
     })
 
 })
+// async
+
+const { readFile, writeFile} = require('fs').promises
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+
+const start=async()=>{
+    try{
+
+        const first = await readFile('./content/first.txt','utf8')
+        const second = await readFile('./content/second.txt','utf8')
+        await writeFile('./content/result-mind-grenades.txt',`This is awesome: ${first} ${second}`,{flag:'a'})
+        console.log(first, second);
+    }catch(err){
+        console.log(err)
+    }
+}
+
+start()
+
+
+
+
+
+// const getText=(path)=>{
+// return new Promise((resolve, reject)=>{
+// readFile(path,'utf8',(err,data)=>{
+//     if(err){
+//         reject(err)
+//     }else{
+//         resolve(data)
+//     }
+// })
+// })
+// }
